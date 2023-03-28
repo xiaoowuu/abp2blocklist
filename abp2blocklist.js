2ed4db4317ff97658048e642ecc7ab0f80df7d2e
xiaoowuu <xiaoowuu@gmail.com>
2023-03-28 17:32:31 +0800

    cmd enable

--- abp2blocklist.js
+++ abp2blocklist.js
@@ -18,33 +18,38 @@
 "use strict";
 
 let readline = require("readline");
-let {Filter} = require("./adblockpluscore/lib/filterClasses");
-let {ContentBlockerList} = require("./lib/abp2blocklist.js");
-
-var rl = readline.createInterface({input: process.stdin, terminal: false});
-var blockerList = new ContentBlockerList({merge: "all"});
-
-rl.on("line", line =>
-{
+let fs = require('fs');
+let { Filter } = require("./adblockpluscore/lib/filterClasses");
+let { ContentBlockerList } = require("./lib/abp2blocklist.js");
+let inputFile = process.argv[2];
+let outputFile = process.argv[3];
+var rl = readline.createInterface({ input: fs.createReadStream(inputFile), terminal: false });
+var blockerList = new ContentBlockerList({ merge: "all" });
+console.log('readInputFile');
+rl.on("line", line => {
   if (/^\s*[^\[\s]/.test(line))
     blockerList.addFilter(Filter.fromText(Filter.normalize(line)));
 });
-
-rl.on("close", () =>
-{
-  blockerList.generateRules().then(rules =>
-  {
+rl.on("close", () => {
+  console.log('generateRules begin');
+  blockerList.generateRules().then(rules => {
     // If the rule set is too huge, JSON.stringify throws
     // "RangeError: Invalid string length" on Node.js. As a workaround, print
     // each rule individually.
-    console.log("[");
-    if (rules.length > 0)
-    {
+    console.log('generateRules end');
+    let content = '';
+    content += "[";
+    if (rules.length > 0) {
       let stringifyRule = rule => JSON.stringify(rule, null, "\t");
       for (let i = 0; i < rules.length - 1; i++)
-        console.log(stringifyRule(rules[i]) + ",");
-      console.log(stringifyRule(rules[rules.length - 1]));
+        content += stringifyRule(rules[i]) + ",";
+      content += stringifyRule(rules[rules.length - 1]);
     }
-    console.log("]");
+    content += "]";
+    console.log('createWriteStream');
+    fs.createWriteStream(outputFile).write(content, ((res) => {
+      console.log(res);
+    }));
+    console.log('complete');
   });
 });
